ef89664daa32006dd137bc7c1bf207a5f54eb77d
kouujaa <kouujaa@gmail.com>
2021-05-18 17:16:46 +0100

    make it better2

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ mongoose.connect(
     console.log("connected to database");
   }
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log("server running on port", PORT);
 });
